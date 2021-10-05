5bf16a632f910f6c3c58f1510e1a9cff4beed830
playerplayer <bohdan.k2000@gmail.com>
2021-10-05 14:25:52 -0400

    Refactored MainActivity

--- app/src/main/java/io/hazylabs/selectactivity/SelectActivity.kt
+++ app/src/main/java/io/hazylabs/selectactivity/SelectActivity.kt
@@ -3,7 +3,7 @@ package io.hazylabs.selectactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SelectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
