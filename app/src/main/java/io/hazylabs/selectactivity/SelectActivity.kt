c05b41d0303b58eba50697e87aaa90a3e3207e93
playerplayer <bohdan.k2000@gmail.com>
2021-10-13 17:45:26 -0400

    Display activity has different name

--- app/src/main/java/io/hazylabs/selectactivity/SelectActivity.kt
+++ app/src/main/java/io/hazylabs/selectactivity/SelectActivity.kt
@@ -24,6 +24,9 @@ class SelectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val txtHint = findViewById<TextView>(R.id.textViewHint)
+        txtHint.text = resources.getString(R.string.hint)
+
         var names = resources.getStringArray(R.array.breeds);
         var descriptions = resources.getStringArray(R.array.descriptions);
 
